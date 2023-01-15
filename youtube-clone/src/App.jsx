80805ec1570d6767867dc048a8168090d58b37e7
Paul Maweu <maweupaul85@gmail.com>
2023-01-15 20:47:54 +0300

    initial commit

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import Navbar from "./components/Navbar";
-import Video from "./components/Video";
-import Channel from "./components/Channel";
-import Search from "./components/Search";
+import { Navbar, Feed, Video, Channel, Search } from "./components";
 const App = () => (
   <Box sx={{ backgroundColor: "black" }}>
     <Navbar />
-    in
     <Routes>
       <Route path="/" exact element={<Feed />} />
       <Route path="/video/:id" element={<Video />} />
