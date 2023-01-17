7e20e1407e84895fba00299ac2ac7a67c128155b
Paul Maweu <maweupaul85@gmail.com>
2023-01-17 19:21:16 +0300

    ...

--- youtube-clone/src/components/Feed.jsx
+++ youtube-clone/src/components/Feed.jsx
@@ -1,7 +1,29 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
+import Typography from "@mui/material/Typography";
+import { Box, Stack } from "@mui/system";
+import Sidebar from "./Sidebar";
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid gold",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 2, color: "white" }}
+        >
+          copyright 2023 Paul Maweu
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 
 export default Feed;
