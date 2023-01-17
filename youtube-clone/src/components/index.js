cf1eaec7ca5df04b6a9ca0e5dce1b0e4cbf98672
Paul Maweu <maweupaul85@gmail.com>
2023-01-17 19:16:04 +0300

    ....

--- youtube-clone/src/components/index.js
+++ youtube-clone/src/components/index.js
@@ -4,3 +4,4 @@ export { default as Feed } from "./Feed";
 export { default as Search } from "./Search";
 export { default as Video } from "./Video";
 export { default as SearchBar } from "./SearchBar";
+export { default as Sidebar } from "./Sidebar";
