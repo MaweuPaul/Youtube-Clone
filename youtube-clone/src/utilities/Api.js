f45ec51c497ce3bac7950eee7213ac1bc5d902e0
Paul Maweu <maweupaul85@gmail.com>
2023-01-19 10:05:55 +0300

    ...

--- youtube-clone/src/utilities/Api.js
+++ youtube-clone/src/utilities/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+const URL = "https://youtube-v31.p.rapidapi.com";
 
 const options = {
   params: {
@@ -10,12 +10,12 @@ const options = {
     maxResults: "50",
   },
   headers: {
-    "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
+    "X-RapidAPI-Key": import.meta.env.VITE_RAPIDAPI_KEY,
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
 
-export const APi = async (url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+export const Api = async (url) => {
+  const { data } = await axios.get(`${URL}/${url}`, options);
   return data;
 };
