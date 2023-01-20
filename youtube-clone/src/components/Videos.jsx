3032efff0a731c2c93f05102179099e2f78d87ac
Paul Maweu <maweupaul85@gmail.com>
2023-01-20 10:30:59 +0300

    ...

--- youtube-clone/src/components/Videos.jsx
+++ youtube-clone/src/components/Videos.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Stack, Box } from "@mui/material";
+import { VideoCard, ChannelCard } from "./index";
 
 const Videos = ({ videos }) => {
-  console.log(videos);
-  return <div>Video</div>;
+  return (
+    <Stack flexWrap="wrap" justifyContent="start" direction="row" gap={2}>
+      {videos.map((item, index) => {
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {/* {item.snippet.channelId && <ChannelCard channelDetail={item} />} */}
+        </Box>;
+      })}
+    </Stack>
+  );
 };
 
 export default Videos;
