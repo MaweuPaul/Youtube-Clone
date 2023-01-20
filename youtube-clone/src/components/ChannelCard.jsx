2b61d70d846318be15aa9c1ced608b28440f373c
Paul Maweu <maweupaul85@gmail.com>
2023-01-20 22:14:51 +0300

    channelCard component

--- youtube-clone/src/components/ChannelCard.jsx
+++ youtube-clone/src/components/ChannelCard.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
-const ChannelCard = () => {
-  return <div>ChannelCard</div>;
+import { Box, Typography, CardMedia, CardContent } from "@mui/material";
+import { Link } from "react-router-dom";
+const ChannelCard = ({ channelDetail }) => {
+  return (
+    <Box sx={{ borderRadius: "20px" }}>
+      <Link to={`/channel/${channelDetail.id.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            textAlign: "center",
+            color: "white",
+          }}
+        >
+          <CardMedia
+            image={channelDetail.snippet.thumbnails.high.url}
+            alt={channelDetail?.snippet.title}
+            sx={{ borderRadius: "50%", height: "180px", width: "180px" }}
+          ></CardMedia>
+        </CardContent>
+      </Link>
+    </Box>
+  );
 };
 
 export default ChannelCard;
