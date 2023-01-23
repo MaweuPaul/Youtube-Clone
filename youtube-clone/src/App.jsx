ce676fe7e34df288c4bd9debae4c5ae08ff2f923
Paul Maweu <maweupaul85@gmail.com>
2023-01-23 20:15:32 +0300

    imports

--- youtube-clone/src/App.jsx
+++ youtube-clone/src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import { Navbar, Feed, Channel, Search, Videos } from "./components";
+import {
+  Navbar,
+  Feed,
+  Search,
+  Videos,
+  ChannelDetail,
+} from "./components/index";
 const App = () => (
   <Box sx={{ backgroundColor: "black" }}>
     <Navbar />
     <Routes>
       <Route path="/" exact element={<Feed />} />
       <Route path="/video/:id" element={<Videos />} />
-      <Route path="/channel/:id" element={<Channel />} />
+      <Route path="/channel/:id" element={<ChannelDetail />} />
       <Route path="/search/:searchTerm" element={<Search />} />
     </Routes>
   </Box>
