82cf4c43e239f794eee15b10546b180570dc515a
Paul Maweu <maweupaul85@gmail.com>
2023-02-04 14:51:33 +0300

    ...

--- youtube-clone/src/components/index.js
+++ youtube-clone/src/components/index.js
@@ -7,3 +7,4 @@ export { default as SearchBar } from "./SearchBar";
 export { default as Sidebar } from "./Sidebar";
 export { default as ChannelCard } from "./ChannelCard";
 export { default as VideoCard } from "./VideoCard";
+export { default as VideoDetails } from "./videoDetails";
